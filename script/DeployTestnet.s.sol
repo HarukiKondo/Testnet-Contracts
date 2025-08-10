11f0f092124bbd7095a6a47d85b30d566d93773d
jistro <contact@jistro.xyz>
2025-08-10 14:55:41 -0600

    refactor: restructure metadata handling and address data in DeployTestnet script

--- script/DeployTestnet.s.sol
+++ script/DeployTestnet.s.sol
@@ -13,35 +13,93 @@ contract DeployTestnet is Script {
     Evvm evvm;
     Estimator estimator;
     NameService nameService;
-    address admin = 0x5cBf2D4Bbf834912Ad0bD59980355b57695e8309;
-    address goldenFisher = 0x5cBf2D4Bbf834912Ad0bD59980355b57695e8309;
-    address activator = 0x5cBf2D4Bbf834912Ad0bD59980355b57695e8309;
-    EvvmStructs.EvvmMetadata evvmMetadata =
-        EvvmStructs.EvvmMetadata({
-            EvvmName: "EVVM",
-            EvvmID: abi.encodePacked(uint256(1)),
-            principalTokenName: "Mate Token",
-            principalTokenSymbol: "MATE",
-            principalTokenAddress: 0x0000000000000000000000000000000000000001,
-            totalSupply: 2033333333000000000000000000,
-            eraTokens: 2033333333000000000000000000 / 2,
-            reward: 5000000000000000000
-        });
+
+    struct AddressData {
+        address activator;
+        address admin;
+        address goldenFisher;
+    }
+
+    struct BasicMetadata {
+        uint256 EvvmID;
+        string EvvmName;
+        string principalTokenName;
+        string principalTokenSymbol;
+    }
+
+    struct AdvancedMetadata {
+        uint256 eraTokens;
+        uint256 reward;
+        uint256 totalSupply;
+    }
 
     function setUp() public {}
 
     function run() public {
+        string memory path = "input/address.json";
+        assert(vm.isFile(path));
+        string memory data = vm.readFile(path);
+        bytes memory dataJson = vm.parseJson(data);
+
+        AddressData memory addressData = abi.decode(dataJson, (AddressData));
+
+        path = "input/evvmBasicMetadata.json";
+        assert(vm.isFile(path));
+        data = vm.readFile(path);
+        dataJson = vm.parseJson(data);
+
+        BasicMetadata memory basicMetadata = abi.decode(
+            dataJson,
+            (BasicMetadata)
+        );
+
+        path = "input/evvmAdvancedMetadata.json";
+        assert(vm.isFile(path));
+        data = vm.readFile(path);
+        dataJson = vm.parseJson(data);
+
+        AdvancedMetadata memory advancedMetadata = abi.decode(
+            dataJson,
+            (AdvancedMetadata)
+        );
+
+        console2.log("Admin:", addressData.admin);
+        console2.log("GoldenFisher:", addressData.goldenFisher);
+        console2.log("Activator:", addressData.activator);
+        console2.log("EvvmName:", basicMetadata.EvvmName);
+        console2.log("EvvmID:", basicMetadata.EvvmID);
+        console2.log("PrincipalTokenName:", basicMetadata.principalTokenName);
+        console2.log(
+            "PrincipalTokenSymbol:",
+            basicMetadata.principalTokenSymbol
+        );
+        console2.log("TotalSupply:", advancedMetadata.totalSupply);
+        console2.log("EraTokens:", advancedMetadata.eraTokens);
+        console2.log("Reward:", advancedMetadata.reward);
+
+        EvvmStructs.EvvmMetadata memory inputMetadata = EvvmStructs
+            .EvvmMetadata({
+                EvvmName: basicMetadata.EvvmName,
+                EvvmID: basicMetadata.EvvmID,
+                principalTokenName: basicMetadata.principalTokenName,
+                principalTokenSymbol: basicMetadata.principalTokenSymbol,
+                principalTokenAddress: 0x0000000000000000000000000000000000000001,
+                totalSupply: advancedMetadata.totalSupply,
+                eraTokens: advancedMetadata.eraTokens,
+                reward: advancedMetadata.reward
+            });
+
         vm.startBroadcast();
 
-        sMate = new Staking(admin, goldenFisher);
-        evvm = new Evvm(admin, address(sMate), evvmMetadata);
+        sMate = new Staking(addressData.admin, addressData.goldenFisher);
+        evvm = new Evvm(addressData.admin, address(sMate), inputMetadata);
         estimator = new Estimator(
-            activator,
+            addressData.activator,
             address(evvm),
             address(sMate),
-            admin
+            addressData.admin
         );
-        nameService = new NameService(address(evvm), admin);
+        nameService = new NameService(address(evvm), addressData.admin);
 
         sMate._setupEstimatorAndEvvm(address(estimator), address(evvm));
         evvm._setupNameServiceAddress(address(nameService));
