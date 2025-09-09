fbf58027ccb7052e5f45445ac52f035d79df8767
jistro <contact@jistro.xyz>
2025-09-09 16:06:24 -0600

    refactor: remove ASCII art comment block from Treasury contract

--- src/contracts/treasury/Treasury.sol
+++ src/contracts/treasury/Treasury.sol
@@ -2,16 +2,6 @@
 // Full license terms available at: https://www.evvm.org/docs/EVVMNoncommercialLicense
 
 pragma solidity ^0.8.0;
-/*  
-888b     d888                   888            .d8888b.                    888                             888    
-8888b   d8888                   888           d88P  Y88b                   888                             888    
-88888b.d88888                   888           888    888                   888                             888    
-888Y88888P888  .d88b.   .d8888b 888  888      888         .d88b.  88888b.  888888 888d888 8888b.   .d8888b 888888 
-888 Y888P 888 d88""88b d88P"    888 .88P      888        d88""88b 888 "88b 888    888P"      "88b d88P"    888    
-888  Y8P  888 888  888 888      888888K       888    888 888  888 888  888 888    888    .d888888 888      888    
-888   "   888 Y88..88P Y88b.    888 "88b      Y88b  d88P Y88..88P 888  888 Y88b.  888    888  888 Y88b.    Y88b.  
-888       888  "Y88P"   "Y8888P 888  888       "Y8888P"   "Y88P"  888  888  "Y888 888    "Y888888  "Y8888P  "Y888                                                                                                          
- */
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeTransferLib} from "@solady/utils/SafeTransferLib.sol";
